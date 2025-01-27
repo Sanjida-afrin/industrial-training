7dbd2c240c29e065746b92c32300ea9b249db5bf
sanjida-afrin <sanjidaafrin2707@gmail.com>
2025-01-27 12:13:06 +0600

    print fibonacci

--- Number.cpp
+++ Number.cpp
@@ -3,10 +3,24 @@ using namespace std;
 
 int main() {
 
-	int i;
-	for(i=1; i<=10; i++) {
-		cout<<i<<"\n";
+	// int i;
+	// for(i=1; i<=10; i++) {
+	// 	cout<<i<<"\n";
         
+	// }
+
+    int f1 = 0, f2 = 1, f3 = 0, n;
+	cin>>n;
+	cout<<"Fibonacci Series: \n";
+	cout<<f1<<"\n"<<f2<<"\n";
+	f3 = f1 + f2;
+
+	while (f3 <= n) {
+		cout<<f3<<"\n";
+
+		f1 = f2;
+		f2 = f3;
+		f3 = f1 + f2;
 	}
 	return 0;
 }
